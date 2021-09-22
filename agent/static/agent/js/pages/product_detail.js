0b6e59d5543eebdad79b8b883553f8e4667574e4
toba96 <tohbyy@gmail.com>
2021-09-22 19:00:29 +0100

    add button to control quantity, align product info, home button

--- agent/static/agent/js/pages/product_detail.js
+++ agent/static/agent/js/pages/product_detail.js
@@ -1,50 +1,68 @@
-$(document).ready(function(){
-    const product = JSON.parse($('#prodData').text());
-    console.log(product)
-    const productCarousel = $(".single-product");
-    const tabs = $("#tabsDiv");
-    let latest = ''
-    let productImages = [];
-    // $.each(product.images, (index,value)=>{
-    //     $("#productImages").append(
-    //         `<div class="item">
-    //             <img src="/media/${value}" alt="${value}">
-    //         </div>`
-    //     )
-    // });
-    // rebuildCarousel(productCarousel)
+$(document).ready(function () {
+	const product = JSON.parse($('#prodData').text());
+	console.log(product);
+	const productCarousel = $('.single-product');
+	const tabs = $('#tabsDiv');
+	const productQty = $('#product-quantity');
+	const minusQty = $('#minus-quantity');
+	minusQty.prop('disabled', true);
+	minusQty.addClass('quantity-btn-disabled');
+	const plusQty = $('#plus-quantity');
+	let latest = '';
+	let productImages = [];
+	// $.each(product.images, (index,value)=>{
+	//     $("#productImages").append(
+	//         `<div class="item">
+	//             <img src="/media/${value}" alt="${value}">
+	//         </div>`
+	//     )
+	// });
+	// rebuildCarousel(productCarousel)
 
+	$.each(product.images, (index, value) => {
+		const item = `<div class="item"><img src="/media/${value}" alt="product"></div>`;
+		addAndRefreshCarousel(productCarousel, item);
+	});
+	console.log(product);
+	$('#name').text(product.name);
+	$('#price').text(commaSeparator(product.agent_price));
+	$('#qty').text(commaSeparator(product.quantity_left.toString()));
 
-    $.each(product.images, (index,value)=>{
-        const item = `<div class="item"><img src="/media/${value}" alt="product"></div>`
-        addAndRefreshCarousel(productCarousel, item);
-    });
-    console.log(product)
-    $("#name").text(product.name);
-    $("#price").text(commaSeparator(product.agent_price));
-    $("#qty").text(commaSeparator(product.quantity_left.toString()));
+	$('#share').on('click', function (e) {
+		e.preventDefault();
+		if (navigator.canShare && navigator.canShare({ files: product.images })) {
+			navigator
+				.share({
+					files: product.images,
+					title: product.name,
+					text: `Photos of ${product.name}.`,
+				})
+				.then(() => console.log('Share was successful.'))
+				.catch((error) => console.log('Sharing failed', error));
+		} else {
+			console.log(`Your system doesn't support sharing files.`);
+		}
+	});
 
-    $("#share").on("click", function (e){
-       e.preventDefault();
-        if (navigator.canShare && navigator.canShare({ files: product.images })) {
-            navigator.share({
-                files: product.images,
-                title: product.name,
-                text: `Photos of ${product.name}.`,
-            })
-                .then(() => console.log('Share was successful.'))
-                .catch((error) => console.log('Sharing failed', error));
-        } else {
-            console.log(`Your system doesn't support sharing files.`);
-        }
-    });
+	$('#purchase').on('click', function (e) {
+		console.log('################');
+		e.preventDefault();
 
-    $("#purchase").on("click", function (e){
-        console.log("################")
-        e.preventDefault();
+		$('#accountDetails').modal('show');
+	});
 
-        $('#accountDetails').modal('show');
-
-    });
-
+	minusQty.on('click', function () {
+		productQty.val(Number(productQty.val()) - 1);
+		minusQty.prop('disabled', productQty.val() == 1 ? true : false);
+		minusQty[productQty.val() == 1 ? 'addClass' : 'removeClass'](
+			'quantity-btn-disabled'
+		);
+	});
+	plusQty.on('click', function () {
+		productQty.val(Number(productQty.val()) + 1);
+		minusQty.prop('disabled', productQty.val() == 1 ? true : false);
+		minusQty[productQty.val() == 1 ? 'addClass' : 'removeClass'](
+			'quantity-btn-disabled'
+		);
+	});
 });
--- agent/static/agent/js/utilities.js
+++ agent/static/agent/js/utilities.js
@@ -1,79 +1,91 @@
 function showErrorMsg(msg) {
-   $('#warning').show();
-   $('#warningMessage').text(msg);
+	$('#warning').show();
+	$('#warningMessage').text(msg);
 }
 
-$(".addComma").on('keyup',function(){
-    const m = commaSeparator($(this).val());
-    $(this).val(m)
+$('.addComma').on('keyup', function () {
+	const m = commaSeparator($(this).val());
+	$(this).val(m);
 });
 
-
-$(".moneyFormat").on('load',function(){
-    console.log("##################")
-    let val = $(this).val() ? $(this).val() : $(this).text();
-    const m = commaSeparator(val);
-    $(this).val(m)
+$('.moneyFormat').on('load', function () {
+	console.log('##################');
+	let val = $(this).val() ? $(this).val() : $(this).text();
+	const m = commaSeparator(val);
+	$(this).val(m);
 });
-
 
 function populateForm(form, data) {
-    $.each(data, function(key, value){
-        $('[name='+key+']', form).val(value);
-    });
+	$.each(data, function (key, value) {
+		$('[name=' + key + ']', form).val(value);
+	});
 }
 
 function isNumber(str) {
-    if (typeof str != "string") return false // we only process strings!
-    return !isNaN(str) && !isNaN(parseFloat(str))
+	if (typeof str != 'string') return false; // we only process strings!
+	return !isNaN(str) && !isNaN(parseFloat(str));
 }
 
 function commaSeparator(nStr) {
-    let nStr1=nStr.replace(/,/g, "");
-    const patt = new RegExp(/(\d+)(\d{3})/);
-    while (patt.test(nStr1)) {
-        nStr1=nStr1.replace(patt, '$1,$2');
-    }
-    return nStr1
+	let nStr1 = nStr.replace(/,/g, '');
+	const patt = new RegExp(/(\d+)(\d{3})/);
+	while (patt.test(nStr1)) {
+		nStr1 = nStr1.replace(patt, '$1,$2');
+	}
+	return nStr1;
 }
 
 function removeCommas(number) {
-    return number.replace(/,/g, "");
+	return number.replace(/,/g, '');
 }
 
 function serializeForm(selector) {
-    let formData = {};
-    const formValues = $(selector).serializeArray();
-    $.each(formValues, (i, { name, value }) => {
-        if (value && value !== 'default') formData[name] = value;
-    });
-    return formData;
+	let formData = {};
+	const formValues = $(selector).serializeArray();
+	$.each(formValues, (i, { name, value }) => {
+		if (value && value !== 'default') formData[name] = value;
+	});
+	return formData;
 }
 
-function showNotify(message, type){
-    const icon = type === 'success' ? 'glyphicon glyphicon-ok' : 'glyphicon glyphicon-warning-sign'
+function showNotify(message, type) {
+	const icon =
+		type === 'success'
+			? 'glyphicon glyphicon-ok'
+			: 'glyphicon glyphicon-warning-sign';
 
-    $.notify(
-        {
-            // options
-            icon: icon,
-            title: '',
-            message: message
-        },
-        {
-            type: type
-        }
-    );
+	$.notify(
+		{
+			// options
+			icon: icon,
+			title: '',
+			message: message,
+		},
+		{
+			type: type,
+		}
+	);
 }
 
 function rebuildCarousel(selector) {
-    selector.trigger('destroy.owl.carousel');
-    selector.owlCarousel();
-    return true;
+	selector.trigger('destroy.owl.carousel');
+	selector.owlCarousel();
+	return true;
 }
 
-function addAndRefreshCarousel(selector, elem){
-    selector.trigger('add.owl.carousel', elem);
-    selector.trigger('refresh.owl.carousel');
-    return true;
+function addAndRefreshCarousel(selector, elem) {
+	selector.trigger('add.owl.carousel', elem);
+	selector.trigger('refresh.owl.carousel');
+	return true;
+}
+
+function readCookie(name) {
+	var nameEQ = name + '=';
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
 }
